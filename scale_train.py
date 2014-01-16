8656bfbb7f4751b0d0749ba47736d719941b4814
Fabian Hirschmann <fabian@hirschm.net>
2014-01-17 00:22:56 +0100

    incremental evaluation (pretty fast!)

--- scale_train.py
+++ scale_train.py
@@ -13,8 +13,13 @@ from pp import PreProcessor, MAPPING
 
 
 TESTING = "--test" in sys.argv
+SIZE = [s for s in sys.argv if s.isdigit()]
+if SIZE:
+    SIZE = int(SIZE[0])
+else:
+    SIZE = -1
 HOLDOUT = 0.05
-BATCH_SIZE = 5000
+BATCH_SIZE = 10000
 
 
 def take(n, iterable):
@@ -22,61 +27,73 @@ def take(n, iterable):
     return list(islice(iterable, n))
 
 
+def evaluate(cls, test_set):
+    tweets, outcomes = zip(*test_set)
+    y = np.array(outcomes)
+    X = vectorizer.fit_transform(tweets)
+
+    y_predicted = cls.predict(X)
+    target_names = ['class 0', 'class 1', 'class 2']
+
+    result = classification_report(y, y_predicted, target_names=target_names).split()
+    return (result[-4], result[-3], result[-2])
+
+
 if __name__ == "__main__":
     with open('split_result.csv', 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow(['size', 'classifier', 'pp', 'precision', 'recall', 'f1_score'])
+        writer.writerow(['size', 'tsize', 'classifier', 'pp', 'precision', 'recall', 'f1_score'])
 
-        # 2^22 = 4194304 < 5292305 (# of rows in tweets.big.db)
-        for exp in (xrange(13, 16) if TESTING else xrange(13, 23)):
-            size = 2 ** exp
+        # Minimal or full preprocessing
+        for full_pp in [True, False]:
 
-            # Minimal or full preprocessing
-            for full_pp in [True, False]:
+            # Train and evaluate with two classifiers
+            for cls in [cls1, cls2]:
+                print("Now training a %s with at most %s instances and %s pp" % (
+                    cls.__class__.__name__, SIZE, "full" if full_pp else "minimal"))
 
-                # Train and evaluate with two classifiers
-                for cls in [cls1, cls2]:
-                    print("Now training a %s with at most %s instances and %s pp" % (
-                        cls.__class__.__name__, size, "full" if full_pp else "minimal"))
+                pp = PreProcessor("tweets.small.db" if TESTING else "tweets.big.db", full_pp)
 
-                    pp = PreProcessor("tweets.small.db" if TESTING else "tweets.big.db", full_pp)
+                # Take at most `size` elements
+                if SIZE > 0:
+                    it = islice(pp.tweets(), SIZE)
+                else:
+                    it = pp.tweets()
 
-                    # Take at most `size` elements
-                    it = islice(pp.tweets(), size)
+                # Hold out HOLDOUT * SIZE instances that will form the test
+                # set. This will not shuffle the data and may lead to
+                # a bad test set if the first HOLDOUT * SIZE instances belong
+                # to the same class. However, due to the nature of our
+                # architecture (generators/lazy evaluation) this is the
+                # only way. For this reason, we just shuffled the sqlite
+                # database beforehand.
+                holdouts = []
+                actual_size = 0
 
-                    # Hold out HOLDOUT * size instances that will form the test
-                    # set. This will not shuffle the data and may lead to
-                    # a bad test set if the first HOLDOUT * size instances belong
-                    # to the same class. However, due to the nature of our
-                    # architecture (generators/lazy evaluation) this is the
-                    # only way. For this reason, we just shuffled the sqlite
-                    # database beforehand.
-                    test_tweets, test_outcomes = zip(*take(int(HOLDOUT * size), it))
-                    y_test = np.array(test_outcomes)
-                    X_test = vectorizer.fit_transform(test_tweets)
-                    actual_size = 0
+                while True:
+                    # Take the next batch
+                    batch = take(int((1 - HOLDOUT) * BATCH_SIZE), it)
 
-                    while True:
-                        batch = take(BATCH_SIZE, it)
-                        if not batch:
-                            # no more items
-                            break
+                    # Append HOLDOUT instances to the training set
+                    holdout = take(int(BATCH_SIZE * HOLDOUT), it)
+                    if not batch or not holdout:
+                        # no more items
+                        break
+                    holdouts.extend(holdout)
 
-                        actual_size += len(batch)
-                        print("\rConsumed: {0} of {1}".format(actual_size, int(size * (1 - HOLDOUT)) + 1), end="")
-                        tweets, outcomes = zip(*batch)
+                    actual_size += len(batch)
+                    print("\rConsumed: {0} instances".format(actual_size), end="")
+                    tweets, outcomes = zip(*batch)
 
-                        y = np.array(outcomes)
-                        X = vectorizer.fit_transform(tweets)
+                    y = np.array(outcomes)
+                    X = vectorizer.fit_transform(tweets)
 
-                        cls.partial_fit(X, y, classes=MAPPING.values())
-                    print()
+                    # Partially fit the instances
+                    cls.partial_fit(X, y, classes=MAPPING.values())
 
-                    y_predicted = cls.predict(X_test)
-                    target_names = ['class 0', 'class 1', 'class 2']
+                    precision, recall, f1 = evaluate(cls, holdouts)
 
-                    result = classification_report(y_test, y_predicted, target_names=target_names).split()
-                    # Take only the averaged scores over the three classes
-                    precision, recall, f1_score = result[-4], result[-3], result[-2]
-                    print("F1:", f1_score)
-                    writer.writerow([actual_size, cls.__class__.__name__, full_pp, precision, recall, f1_score])
+                    # Write intermediate results to file
+                    writer.writerow([actual_size, len(holdouts), cls.__class__.__name__, full_pp, precision, recall, f1])
+
+                print("F1:", f1)

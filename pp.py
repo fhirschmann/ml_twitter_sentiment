f5772787f5548fab79a1e2a815b44fd2ef64f2b3
Dominik Schreiber <domischreib@gmail.com>
2014-01-16 18:29:15 +0100

    inserted preprocessing from TweetProcessor into generator-version

--- pp.py
+++ pp.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import re
 
 MAPPING = {
     ':)': 0,
@@ -17,6 +17,7 @@ class PreProcessor(object):
     """
     def __init__(self, db, amount=None, full_pp=True):
         connection = sqlite3.connect(db)
+        self.full_pp = full_pp
 
         if amount:
             self.query = connection.execute('SELECT search_term, text FROM tweets LIMIT 0, %d' % amount)
@@ -27,25 +28,40 @@ class PreProcessor(object):
         while True:
             res = self.query.fetchone()
             if not res:
+                # no more tweets from query
                 break
 
             tweet = res[1]
             cls = MAPPING[res[0]]
 
             if "RT @" in tweet:
-                # we just don't yield retweets
+                # retweet -> don't yield it
                 continue
 
             else:
                 words = tweet.replace('"', '').replace('\n', ' ').split(' ')
                 if len(words) == 0:
+                    # tweet is empty -> don't yield it
                     continue
+                clean = []
+                for word in words: clean += self.process_word(word)
 
-                # blah blah
-                yield (tweet, cls)
+                yield (' '.join(clean), cls)
 
+    def process_word(self, word):
+        if len(word) > 0:
+            if self.full_pp:
+                if word[0] == '@': return [u'<user>']
+                if word[0] == '#': return [w.lower() for w in re.sub(r'([a-z])([A-Z])', r'\g<1> \g<2>', word[1:]).split(' ')]
+                if word[:4] == 'http': return [u'<url>']
+            for mood in [u'\u2665', r'&lt;3+', r'&amp;lt;3+', r'<3+', r'[^\(]*\)+', r'.*\(+', r':.*']:
+                word = re.sub(mood, '', word)
+            if len(word) > 0: return [word.lower()]
+            else: return []
+        else:
+            return []
 
 if __name__ == "__main__":
     pp = PreProcessor("tweets.small.db", 100)
-    for tweet in pp.tweets():
-        print(tweet)
+    for tweet, outcome in pp.tweets():
+        print outcome, '->', tweet

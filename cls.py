5eb13ba867d182439399f0ea78d5c88d51f509fc
Fabian Hirschmann <fabian@hirschm.net>
2014-01-16 15:47:42 +0100

    remove old cls

--- cls.py
+++ cls.py
@@ -2,5 +2,4 @@ from sklearn.linear_model import RidgeClassifier, SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
 
 cls1 = SGDClassifier(loss="log", n_jobs=-1)
-cls2 = RidgeClassifier()
 cls2 = MultinomialNB()

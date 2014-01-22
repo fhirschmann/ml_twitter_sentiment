2df9c8f65824fc50cd14a194247b106540279b05
Wuhn <bungobang@yahoo.de>
2014-01-22 15:37:47 +0100

    Order by Random in split_data anstatt shuffle

--- split_data.py
+++ split_data.py
@@ -17,11 +17,12 @@ def split(inputDB, trainDB, testDB, length):
     
     trainCursor.execute("CREATE TABLE tweets('search_term' varchar(255), 'created_at' varchar(255), 'from_user' varchar(255), 'from_user_id' varchar(255), 'from_user_name' varchar(255), 'geo' varchar(255),'id' varchar(255), 'in_reply_to_status_id' varchar(255), 'iso_language_code' varchar(255), 'source' varchar(255), 'text' varchar(255), 'to_user' varchar(255), 'to_user_id' varchar(255), 'to_user_name' varchar(255))")
     testCursor.execute("CREATE TABLE tweets('search_term' varchar(255), 'created_at' varchar(255), 'from_user' varchar(255), 'from_user_id' varchar(255), 'from_user_name' varchar(255), 'geo' varchar(255),'id' varchar(255), 'in_reply_to_status_id' varchar(255), 'iso_language_code' varchar(255), 'source' varchar(255), 'text' varchar(255), 'to_user' varchar(255), 'to_user_id' varchar(255), 'to_user_name' varchar(255))")
-    inputCursor.execute("SELECT * FROM tweets")
+    inputCursor.execute("SELECT * FROM tweets ORDER BY RANDOM()")
     
     while bool(inputCursor.fetchone()):
         tweets = [tweet for tweet in inputCursor.fetchmany(100000)]
-        numpy.random.shuffle(tweets)
+        # No shuffle needed for random ordering
+        #numpy.random.shuffle(tweets)
         
         #Setting Percentage of the split
         split = int(len(tweets)/2)

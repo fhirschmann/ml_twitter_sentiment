22b8d64e7e463173d64ca29bc586181ddbf55bc8
Dominik Schreiber <domischreib@gmail.com>
2014-01-27 17:46:46 +0100

    added vectorizing, classifying and raw_input-reading. for some reason it predicts word-wise, not tweet-wise. i'll have a look at it in the evening.

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -4,47 +4,45 @@
 # Group members: Dominik Schreiber, Ji-Ung Lee, Fabian Hirschmann
 #
 
-import sqlite3
-import csv
 import pickle
 import numpy as np
 
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.metrics import f1_score
-from sklearn import svm
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn import cross_validation
-from sklearn.grid_search import GridSearchCV
-from nltk.classify.scikitlearn import SklearnClassifier
-from nltk import word_tokenize
-
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_regression
-
+from sklearn.naive_bayes import MultinomialNB
+from vect import vectorizer
+from pp import PreProcessor, MAPPING
 
 class TwitterSentiment:
     def __init__(self):
-        #set all needed variables and parameters here
-        #self.param = 'something'
-        #self.vectorizer = ...
-        #self.classifier = ...
-        pass
+        self.vectorizer = vectorizer
+        self.classifier = MultinomialNB()
+        self.preprocessor = PreProcessor("tweets.small.db", True)
+        X, y = self.transform(self.preprocessor.tweets())
+        self.classifier.fit(X, y)
+
+    def transform(self, batch):
+        tweets, outcomes = zip(*batch)
+        y = np.array(outcomes)
+        X = self.vectorizer.fit_transform(tweets)
+        return X, y
 
     def predict(self, text):
         '''predict an emoticon for any string given by text by using a trained classifier'''
-        return  #0 for :), 1 for :(, 2 for <3
+        clean = []
+        words = text.replace('"', '').replace('\n', ' ').split(' ')
+        for word in words: clean += self.preprocessor.process_word(word)
+        return self.classifier.predict(self.vectorizer.fit_transform(np.array(clean)))
 
     def predict_all(self, seq):
         '''predict all emoticons for a list of strings by using a trained classifier'''
-        return  #list of predictions
+        return [self.predict(str) for str in seq]
 
 
 if __name__ == '__main__':
     ts = TwitterSentiment()
-
+    text = raw_input('How may I serve you, humble master?\n')
+    while text is not 'q':
+        print ts.predict(text)
+        text = raw_input('How may I serve you, humble master? [q to quit]\n')
+    print 'Master has given Dobby a sock. Dobby is free.'
     #you can vectorize your TwitterSentiment class by using pickle:
     #pickle.dump(ts, 'TwitterSentiment.pickle')
